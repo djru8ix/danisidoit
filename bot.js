7d96801af98e7eb2899ab85f45a4920687ae1ecb
djru8ix <51488791+djru8ix@users.noreply.github.com>
2020-04-05 12:54:31 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+
+
+client.login(process.env.f7faRA501u9mhoLfU707xUxvx0sdDjfJ);
